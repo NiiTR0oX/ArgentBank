894a3fc680e234c2ec226eaf6e0a08f79b40550c
NiiTR0oX <benjamin.leblanc3426@hotmail.com>
2024-12-17 15:30:44 +0100

    Update user structuring

--- argentbank/src/pages/User/User.jsx
+++ argentbank/src/pages/User/User.jsx
@@ -28,25 +28,54 @@ export default function User() {
 
   // rajouter la logique de redux sur le dispatch
 
+
+
+
   return (
-    <main className="main bg-dark">
-      <div className="user-header">
-        {/* <h1>Welcome back, {user.firstName}!</h1> */}
-        {/* <button className="edit-button">Edit Name</button> APPELER EDIT NAME */}
-      </div>
-      <section className="user-details">
-        {/* <EditName /> */}
-        <p>
-          {/* <strong>Full Name:</strong> {user.firstName} {user.lastName} */}
-        </p>
-        <p>
-          {/* <strong>Email:</strong> {user.email} */}
-        </p>
-      </section>
-      <section>
-        {/* Je dois appeler 3x les transaction  */}
-        <Transaction title='Argent Bank Savings (x6712)' amount='$10,928.42' description='Available Balance' />
-      </section>
-    </main>
+    <>
+      {/* Barre de navigation */}
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="./user.html">
+            <i className="fa fa-user-circle"></i>
+            Tony
+          </a>
+          <a className="main-nav-item" href="./index.html">
+            <i className="fa fa-sign-out"></i>
+            Sign Out
+          </a>
+        </div>
+      </nav>
+  
+      {/* Contenu principal */}
+      <main className="main bg-dark">
+        {/* En-tête utilisateur */}
+        <div className="header">
+          <h1>Welcome back<br />Tony Jarvis!</h1>
+          <button className="edit-button">Edit Name</button>
+        </div>
+  
+        {/* Détails utilisateur */}
+        <section className="user-details">
+          <p>
+            {/* <strong>Full Name:</strong> {user.firstName} {user.lastName} */}
+          </p>
+          <p>
+            {/* <strong>Email:</strong> {user.email} */}
+          </p>
+        </section>
+  
+        {/* Transactions */}
+        <section>
+          <Transaction title="Argent Bank Checking (x8349)" amount="$2,082.79" description="Available Balance" />
+          <Transaction title="Argent Bank Savings (x6712)" amount="$10,928.42" description="Available Balance" />
+          <Transaction title="Argent Bank Credit Card (x8349)" amount="$184.30" description="Available Balance" />
+        </section>
+      </main>
+    </>
   );
+  
 }
